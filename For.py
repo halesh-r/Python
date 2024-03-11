765b53184bd9a86f138cb82cf053284010631435
Halesh R <haleshrajoli@gmail.com>
2024-03-11 08:40:31 +0530

    For loop concept

---
+++ For.py
@@ -0,0 +1,22 @@
+name="halzz"
+for n in name:
+    print(n)
+
+colors=["red","yellow","green"]   # list
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+
+#  range in for loop
+        
+
+for i in range(5):  #  default 0 to 5 are printed
+   # print(i)       
+    print(i+1)     # i+1 gives  1 to  5
+
+# range(1,n)  1 to n-1 are printed
+
+
+#range(m,n,o)  # m=start n=end o=skip  try karo ek bar
